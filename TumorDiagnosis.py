9a7fff76c7678e9b42d4e97a7dc5bbd1d6ef6723
Balaji Ramesh <30927538+balaji025@users.noreply.github.com>
2023-04-28 20:19:59 +0530

    Update TumorDiagnosis.py

--- TumorDiagnosis.py
+++ TumorDiagnosis.py
@@ -1,5 +1,6 @@
 #importing pandas to read the csv file
 import pandas as pd
+import numpy as np
 dataset = pd.read_csv('cancer.csv')
 
 x = dataset.drop(columns=["diagnosis(1=m, 0=b)"])     #x as every column except diagnosis column
@@ -15,16 +16,36 @@ import tensorflow as tf
 #using a sequential model
 model = tf.keras.models.Sequential()
 
-#adding the data to the neural network layers
+# The core idea of Sequential API is simply arranging the Keras layers in a sequential order and so, it is called Sequential API. 
+# Most of the ANN also has layers in sequential order and the data flows from one layer
+# to another layer in the given order until the data finally reaches the output layer.
+
 model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape[1:], activation='sigmoid'))
+#model.add(tf.keras.layers.Dense(256, input_shape=(x_train.shape[1],), activation='sigmoid'))
 model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+#model.add(tf.keras.Input(shape=(x_train.shape[1],))) 
+#model.add(tf.keras.layers.Dense(128, activation='sigmoid')) 
+#model.add(tf.keras.layers.Dense(256, activation='sigmoid')) 
+#model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
-#to compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-#to train them model
+# Adam optimization is a stochastic gradient descent method that is based on adaptive estimation of first-order and second-order moments.
+# Computes the cross-entropy loss between true labels and predicted labels.
+# Accuracy metric: Calculates how often predictions equal labels.
+# labels = training set
+# pred = predictions made
+
 model.fit(x_train, y_train, epochs=1000)
 
-#to test the model
 model.evaluate(x_test, y_test)
+
+# Evaluation is a process during development of the model to check whether the model is best fit for 
+# the given problem and corresponding data. Keras model provides a function, evaluate which does the evaluation of the model.
+
+# to make prediction for a given input
+features = np.array([[17.99,10.38,122.8,1001,0.1184,0.2776,0.3001,0.1471,0.2419,0.07871,1.095,0.9053,8.589,153.4,0.006399,0.04904,
+                      0.05373,0.01587,0.03003,0.006193,25.38,17.33,184.6,2019,0.1622,0.6656,0.7119,0.2654,0.4601,0.1189]])
+j=model.predict(features)
+print(j)
